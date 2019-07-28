7f2eaccf90f6bf7785e1702666b2a5e79e528eb5
Matt Light <matt@paren.io>
2019-07-27 18:45:41 -0700

    Update exports prior to npm publish

--- rollup.config.js
+++ rollup.config.js
@@ -2,10 +2,15 @@ const pkg = require('./package.json');
 
 export default [{
   input: 'src/index.js',
-  output: {
-    file: pkg.main,
-    format: 'cjs',
-    sourcemap: true,
-  },
+  output: [
+    {
+      file: pkg.main,
+      format: 'cjs'
+    },
+    {
+      file: pkg.module,
+      format: 'esm'
+    },
+  ],
   external: ['fs', 'util'],
 }];
